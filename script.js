6e71da0bc1519314e25f855ef869f9b9f6c50333
Vick-Bay <levicking@hotmail.com>
2019-08-05 00:05:32 +0400

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,29 @@ image1 = document.querySelector(".img1");
 
 switch(randomNumber1){
 
-  case 1:
-      image1.setAttribute("src","/images/dice1.png");
-      break;
+    case 1:
+        image1.setAttribute("src","/images/dice1.png");
+        break;
 
-  case 2:
-      image1.setAttribute("src","/images/dice2.png");
-      break;
+    case 2:
+        image1.setAttribute("src","/images/dice2.png");
+        break;
+
+    case 3:
+        image1.setAttribute("src","/images/dice3.png");
+        break;
+  
+    case 4:
+        image1.setAttribute("src","/images/dice4.png");
+        break;
+
+    case 5:
+        image1.setAttribute("src","/images/dice5.png");
+        break;
+
+    case 6:
+        image1.setAttribute("src","/images/dice6.png");
+        break;
 }
 
 
