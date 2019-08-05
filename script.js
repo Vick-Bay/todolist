310f93ca86b107f22057a270e2284df8e0cb6602
Vick-Bay <50940714+Vick-Bay@users.noreply.github.com>
2019-08-05 09:22:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,39 @@
 
 // First Random number between 1 and 6
-var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 
 //Select Image 1
 image1 = document.querySelector(".img1");
 
 //Manipulate Image 1
-switch(randomNumber1){
-
-    case 1:
-        image1.setAttribute("src","/images/dice1.png");
-        break;
-
-    case 2:
-        image1.setAttribute("src","/images/dice2.png");
-        break;
-
-    case 3:
-        image1.setAttribute("src","/images/dice3.png");
-        break;
-  
-    case 4:
-        image1.setAttribute("src","/images/dice4.png");
-        break;
-
-    case 5:
-        image1.setAttribute("src","/images/dice5.png");
-        break;
-
-    case 6:
-        image1.setAttribute("src","/images/dice6.png");
-        break;
-}
-
+image1.setAttribute("src", "/images/dice" + randomNumber1 +  ".png");
 
 // Second Random number between 1 and 6
-var randomNumber2 = Math.floor(Math.random() * 6) + 1 ;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 
 //Select Image 2
 image2 = document.querySelector(".img2");
 
 //Manipulate Image 2
-switch(randomNumber2){
+image2.setAttribute("src", "/images/dice" + randomNumber2 +  ".png");
 
-    case 1:
-        image2.setAttribute("src","/images/dice1.png");
-        break;
+if (randomNumber1 > randomNumber2) {
 
-    case 2:
-        image2.setAttribute("src","/images/dice2.png");
-        break;
+    document.querySelector("h1").innerHTML = "Player 1 Wins!"
+}
 
-    case 3:
-        image2.setAttribute("src","/images/dice3.png");
-        break;
-  
-    case 4:
-        image2.setAttribute("src","/images/dice4.png");
-        break;
+else if (randomNumber2 > randomNumber1) {
 
-    case 5:
-        image2.setAttribute("src","/images/dice5.png");
-        break;
+    document.querySelector("h1").innerHTML = "Player 2 Wins!"
 
-    case 6:
-        image2.setAttribute("src","/images/dice6.png");
-        break;
+}
+
+else {
+
+    document.querySelector("h1").innerHTML = "Draw!"
+
 }
 
 
