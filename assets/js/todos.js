6457c41b2cbeb2984c8a39322669b95999dd4cde
Vick-Bay <levicking@hotmail.com>
2019-08-17 21:32:45 +0400

    update

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,5 +1,28 @@
 // Check off specific Todos by clicking
 
-$("li").click(function(){
+$("ul").on("click","li", function(){
   $(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    //get new todo text from input
+    var toDoText = $(this).val();
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> "+ toDoText + "</li>");
+  }
+});
+
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle();
 });
